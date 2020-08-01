2f8e1af60de683420fe68b923977dbd46342b5a4
MooQuackOink <MooQuackOink@users.noreply.github.com>
2020-07-31 23:40:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,18 +16,16 @@ bot.on('message', msg =>{
     switch(args[0]){
 
         case '$help':
-            if(args[1] === 'info'){
-                msg.channel.send('Current Event: Word Hunt\nSolve 3 riddles and piece a phrase to win. To submit an answer, put a $ before each answer (all answers should be 1 word, all lowercase). You will get a DM when you get a correct answer, giving you part of the phrase. First to piece together all three phrases win!'); 
-            }else {
             const embed = new Discord.MessageEmbed()
             .setTitle('Command List')
             .addFields(
-                {name:'Event Commands', value:"$riddle (number)\nGet a riddle for the word hunt (1, 2, or 3)\n"},
-                {name:'Other Commands', value:"$help\nShows the list of commands\n\n$help info\nShows the current ongoing event\n\n$subscribe\nCreator's YT link", inline:true}
+                {name:'Basic Commands', value:"$help\nShows the list of commands\n\n", inline:true}
+            )
+            .addFields(
+                {name:'Strike Commands', value:"$strikes\nShows how many strikes you have and why\n\n", inline:true}
             )
                 .setColor('#68e3a8')
             msg.channel.send(embed);
-            }
             break;
         case '$beeptest':
             msg.channel.send("Beep").then((sent) => sent.edit("Boop!"))
@@ -38,7 +36,7 @@ bot.on('message', msg =>{
             }, 5000);
             break;
         case "$dmtest":
-            msg.author.send("Is this your dms?");
+            msg.author.send("Yo");
             break;
         case "$embed":
             const embed = new Discord.MessageEmbed()
@@ -47,11 +45,17 @@ bot.on('message', msg =>{
             .setColor('#48C9B0')
             msg.channel.send(embed);
             break;
+        
+        //strike commands
+        case "$strikes":
+            const embed = new Discord.MessageEmbed()
+            .setTitle(msg.author.username + '\'s strikes')
+            .addField('Strike 1', "Reason for strike")
+            .setColor('#c40d00')
+            msg.channel.send(embed);
+            break;
         }
 })
-
-
-
 
 const usedButton = new Set();
 
